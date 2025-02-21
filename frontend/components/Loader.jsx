2a9197a2dfc90c86085df7fe26a012c3609a4939
Ankit Mani Pandey <167631293+ankitmanipandey@users.noreply.github.com>
2025-02-22 01:54:42 +0530

    Update Loader.jsx

--- frontend/components/Loader.jsx
+++ frontend/components/Loader.jsx
@@ -3,7 +3,7 @@ import { Bars } from 'react-loader-spinner'
 
 export default function Loader() {
     return (
-        <div className='flex justify-center items-center h-screen bg-transparent'>
+        <div className='flex justify-center items-center h-screen fixed w-full bg-transparent'>
             <Bars
                 height="80"
                 width="80"
