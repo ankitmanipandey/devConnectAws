0bdf4c88ec4c1ea3f8f5755647d63160f34c12ac
Ankit Mani Pandey <167631293+ankitmanipandey@users.noreply.github.com>
2025-02-25 14:38:11 +0530

    Update constants.js

--- backend/utils/constants.js
+++ backend/utils/constants.js
@@ -1,3 +1,3 @@
 const ICON_URL = "https://imgs.search.brave.com/CwbNAP30Xf-1h9SHkiTkAK5-TAiU5WZGZg2PwfXypy0/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9tZWRp/YS5nZXR0eWltYWdl/cy5jb20vaWQvNDYz/MTA0NDE0L3Bob3Rv/L2FkZWxhaWRlLWF1/c3RyYWxpYS1tcy1k/aG9uaS1vZi1pbmRp/YS1wb3Nlcy1kdXJp/bmctdGhlLWluZGlh/LTIwMTUtaWNjLWNy/aWNrZXQtd29ybGQt/Y3VwLmpwZz9zPTYx/Mng2MTImdz0wJms9/MjAmYz1BcUhMQ012/cHBQWjdRMmxoRG9m/VndValBtcjZPaV9h/WkQ4MUduVU5Za3I4/PQ"
-const FRONTEND_URL="http://13.48.29.200"
+const FRONTEND_URL="http://16.16.144.114"
 module.exports = { ICON_URL,FRONTEND_URL }
