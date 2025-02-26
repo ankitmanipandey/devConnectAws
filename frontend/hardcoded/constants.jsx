0602e8510eafea18030902c2e5926eb53a2b6ea7
Ankit Mani Pandey <ankitpandey78600@gmail.com>
2025-02-26 15:31:01 +0530

    changed the frontend and backend url

--- frontend/hardcoded/constants.jsx
+++ frontend/hardcoded/constants.jsx
@@ -1,4 +1,4 @@
-export const BACKEND_URL = "http://localhost:1111"
+export const BACKEND_URL = "http://13.60.224.138:1111"
 export const BACKGROUND_IMAGE = "/loginpage_bg.jpg"
 export const DEFAULT_PHOTOURL = "https://t4.ftcdn.net/jpg/01/19/32/93/240_F_119329387_sUTbUdeyhk0nuhNw5WaFvOyQFmxeppjX.jpg"
 export const CLOUDINARY_URL = "https://api.cloudinary.com/v1_1/daiiyb5u4/image/upload"
