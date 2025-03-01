4918f880712806b52abe5b415357f046b6c99d97
Ankit Mani Pandey <ankitpandey78600@gmail.com>
2025-03-02 03:37:57 +0530

    trying to debug

--- backend/routes/paymentWebhook.js
+++ backend/routes/paymentWebhook.js
@@ -6,7 +6,7 @@ const stripe = Stripe(process.env.STRIPE_SECRET_KEY);
 const PaymentModel = require('../model/paymentData')
 
 
-paymentWebhookRouter.post('/webhook', express.raw({ type: 'application/json' }), async (req, res) => {
+paymentWebhookRouter.post('/webhook', { type: 'application/json' }, async (req, res) => {
     let event = req.body;
     const endpointSecret = process.env.STRIPE_WEBHOOK_SECRET;
     if (endpointSecret) {
@@ -21,28 +21,28 @@ paymentWebhookRouter.post('/webhook', express.raw({ type: 'application/json' }),
             return res.status(400).send(err.message)
         }
     }
-    console.log("no error in signature")
-    const session = event.data.object //this we get from the object 'event'
-    console.log(event.type)
-    console.log(typeof (event.type) === string)
-    if (event.type === 'checkout.session.completed') {
-        try {
-            console.log("checkout started")
-            const payment = await PaymentModel.create({
-                orderId: session.id,
-                userName: session.metadata.userName,
-                userId: session.metadata.userId,
-                membershipType: session.metadata.membershipType,
-                price: (session.amount_total) / 100
-            })
-            console.log("data saved in database")
-            return res.status(200).json({ success: true, message: "Payment processed successfully" })
-        }
-        catch (err) {
-            return res.status(400).json({ success: false, message: "Transaction failed" + err.message })
-        }
-    }
-    return res.status(200).json({ received: true });
+    // console.log("no error in signature")
+    // const session = event.data.object //this we get from the object 'event'
+    // console.log(event.type)
+    // console.log(typeof (event.type) === string)
+    // if (event.type === 'checkout.session.completed') {
+    //     try {
+    //         console.log("checkout started")
+    //         const payment = await PaymentModel.create({
+    //             orderId: session.id,
+    //             userName: session.metadata.userName,
+    //             userId: session.metadata.userId,
+    //             membershipType: session.metadata.membershipType,
+    //             price: (session.amount_total) / 100
+    //         })
+    //         console.log("data saved in database")
+    //         return res.status(200).json({ success: true, message: "Payment processed successfully" })
+    //     }
+    //     catch (err) {
+    //         return res.status(400).json({ success: false, message: "Transaction failed" + err.message })
+    //     }
+    // }
+    // return res.status(200).json({ received: true });
 });
 
 module.exports = paymentWebhookRouter
