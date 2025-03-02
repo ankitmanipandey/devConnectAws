ef49b99d338e53cd8c97298973cfdac2fcde8633
Ankit Mani Pandey <ankitpandey78600@gmail.com>
2025-03-02 06:35:01 +0530

    made some changes

--- backend/routes/payment.js
+++ backend/routes/payment.js
@@ -1,51 +1,54 @@
-    const express = require('express')
-    const Stripe = require("stripe");
-    const userAuth = require('../middlewares/userAuth');
-    const paymentRouter = express.Router()
-    require('dotenv').config()
-    const stripe = Stripe(process.env.STRIPE_SECRET_KEY);
+const express = require('express')
+const Stripe = require("stripe");
+const userAuth = require('../middlewares/userAuth');
+const paymentRouter = express.Router()
+require('dotenv').config()
+const stripe = Stripe(process.env.STRIPE_SECRET_KEY);
 
 
 
-    paymentRouter.post("/create-checkout-session/:type", userAuth, async (req, res) => {
-        try {
-            const planType = req.params.type.toString()
-            const user = req.user
-            const PRODUCT = {
-                name: planType === 'silver' ? "Silver Premium Membership" : "Gold Premium Membership",
-                price: planType === 'silver' ? 500 : 1000,
-                currency: "INR",
-            };
-            const session = await stripe.checkout.sessions.create({
-                payment_method_types: ["card"],
-                mode: "payment",
-                line_items: [
-                    {
-                        price_data: {
-                            currency: PRODUCT.currency,
-                            product_data: {
-                                name: PRODUCT?.name,
-                            },
-                            unit_amount: PRODUCT.price * 100,
+paymentRouter.post("/create-checkout-session/:type", userAuth, async (req, res) => {
+    try {
+        const planType = req.params.type.toString()
+        console.log("starting the checkout")
+        const user = req.user
+        const PRODUCT = {
+            name: planType === 'silver' ? "Silver Premium Membership" : "Gold Premium Membership",
+            price: planType === 'silver' ? 500 : 1000,
+            currency: "INR",
+        };
+        console.log("before the session")
+        const session = await stripe.checkout.sessions.create({
+            payment_method_types: ["card"],
+            mode: "payment",
+            line_items: [
+                {
+                    price_data: {
+                        currency: PRODUCT.currency,
+                        product_data: {
+                            name: PRODUCT?.name,
                         },
-                        quantity: 1,
+                        unit_amount: PRODUCT.price * 100,
                     },
-                ],
-                metadata: {
-                    userName: user?.name?.toString(),
-                    userId: user?._id.toString(),
-                    membershipType: planType,
-                    additional_info: "Special discount applied"
+                    quantity: 1,
                 },
-                success_url: "http://localhost:5173/premium",
-                cancel_url: "http://localhost:5173/premium",
-            });
-            res.json({ id: session.id });
-        } catch (error) {
-            res.status(500).json({ error: error.message });
-        }
-    });
+            ],
+            metadata: {
+                userName: user?.name?.toString(),
+                userId: user?._id.toString(),
+                membershipType: planType,
+                additional_info: "Special discount applied"
+            },
+            success_url: "http://localhost:5173/premium",
+            cancel_url: "http://localhost:5173/premium",
+        });
+        console.log("after the session")
+        return res.json({ id: session.id });
+    } catch (error) {
+        return res.status(500).json({ error: error.message });
+    }
+});
 
 
 
-    module.exports = paymentRouter
+module.exports = paymentRouter
