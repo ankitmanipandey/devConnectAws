4ed50a519e8479bb8e18dc02091cb78b5fdae942
Ankit Mani Pandey <ankitpandey78600@gmail.com>
2025-03-11 14:34:17 +0530

    made some changes

--- frontend/hardcoded/socket.jsx
+++ frontend/hardcoded/socket.jsx
@@ -3,6 +3,7 @@ const BACKEND_URL = import.meta.env.VITE_BACKEND_URL
 export const createSocketConnection = () => {
     return io(BACKEND_URL, {
         path: '/socket.io',
-        transports: ['websocket']
+        transports: ['websocket'],
+        withCredentials: true
     })
 }
